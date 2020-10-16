58f1200728fabeae2f9e605f831eebc9392123ff
Kavin Tjhan <tjhan.kavin@gmail.com>
2020-10-15 20:08:53 -0700

    finished

---
+++ src/Snackbar/VendingMachine.java
@@ -0,0 +1,34 @@
+package Snackbar;
+
+public class VendingMachine {
+    
+    private static int maxId = 0;
+    private int id;
+    private String name;
+
+    public VendingMachine(String name)
+    {
+        maxId++;
+        id= maxId;
+        this.name= name;
+
+
+    }
+    // getter
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    //setter
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
